028dd273fee514c7a3629571e9b8bd157f8c3953
abednego emonena <emonenaabednego@gmail.com>
2024-10-12 15:40:59 +0100

    Refactor: Add loader component and loading state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import { Footer } from './components/Footer'
 import { Info } from './components/Info'
+import { Loader } from './components/Loader'
 
 function App() {
   const [title, setTitle] = useState("")
   const [backgroundImage, setbackgroundImage] = useState("")
   const [infoBar, setInfoBar] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const [description, setDescription] = useState("")
   const [date, setDate] = useState("")
@@ -13,20 +15,20 @@ function App() {
   async function fetch_data() {
     const NASA_API_KEY = import.meta.env.VITE_META_API_KEY;
     const url = "https://api.nasa.gov/planetary/apod" + `?api_key=${NASA_API_KEY}`;
-    console.log(NASA_API_KEY)
-    console.log(url)
 
     try {
+      setIsLoading(true)
       const response = await fetch(url)
       const data = await response.json()
       setTitle(data.title)
       setDescription(data.explanation)
       setbackgroundImage(data.hdurl)
       setDate(data.date)
-      console.log(data)
+      setIsLoading(false)
     } catch {
       console.log("error")
     }
+
   }
 
   useEffect(() => {
@@ -35,10 +37,13 @@ function App() {
 
   return (
     <>
-      <main style={{backgroundImage: `url(${backgroundImage})`}}>
+      {
+        isLoading?<Loader/>
+        :<main style={{backgroundImage: `url(${backgroundImage})`}}>
         <Info infoBar ={infoBar} setInfoBar={setInfoBar} title={title} date={date} description={description}/>
         <Footer setInfoBar={setInfoBar} title={title}/>
       </main>
+      }
     </>
   )
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Loader = () => {
+  return (
+    <div className='loader'>
+        <i className="fa-solid fa-gear"></i>
+    </div>
+  )
+}
